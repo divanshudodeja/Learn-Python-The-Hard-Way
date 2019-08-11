dbb1d5ca58791ccccd4a49e801d956dce6e72563
Divanshu Dodeja <divanshu.dodeja@gmail.com>
2019-08-11 08:32:49 +0530

    Exercise 17

---
+++ 17_Exercise21.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    print("Adding %d + %d" %(a, b))
+    return a + b
+
+def subtract(a, b):
+    print("Subtracting %d - %d" %(a, b))
+    return a - b
+
+age = add(30, 5)
+height = subtract(78, 4)
