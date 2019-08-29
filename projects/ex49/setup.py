b3c27b68119112e51ad1cddc5382fa7da5d1cd6e
Divanshu Dodeja <divanshu.dodeja@gmail.com>
2019-08-29 07:45:04 +0530

    ex49 added

---
+++ projects/ex49/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import  setup
+
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description' : 'My Project',
+    'author' : 'Divanshu Dodeja',
+    'url' : 'http://github.com/divanshudodeja',
+    'author_email' : 'divanshu.dodeja@gmail.com',
+    'version' : '0.1',
+    'install_requires' : ['nose'],
+    'packages' : ['ex49'],
+    'scripts' : [],
+    'name' : 'projectname'
+}
+
+setup(**config)
