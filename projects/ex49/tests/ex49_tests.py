1a41ecbb5aeae678f218914e9eeff285e911b340
Divanshu Dodeja <divanshu.dodeja@gmail.com>
2019-08-29 09:11:09 +0530

    Created ex49_test.py

---
+++ projects/ex49/tests/ex49_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import assert_equal
+from ex49 import ex49
+
+def test_peek():
+    assert_equal(ex49.peek([('verb', 'go')]), 'verb')
+    assert_equal(ex49.peek([]), None)
+
+def test_match():
+    assert_equal(ex49.match([], 'verb'), None)
+    assert_equal(ex49.match([('verb', 'go')], 'verb'), ('verb', 'go'))
+
